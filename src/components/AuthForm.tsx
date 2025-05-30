3fc8e4c0d81d492d135934ed9a8baca3a334a337
colombefioren <colomberakotonjanahary@gmail.com>
2025-05-30 16:48:16 +0300

    refactor: std input label

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -99,7 +99,7 @@ const AuthForm = () => {
             </div>
             <div>
               <label htmlFor="std" className="block text-sm font-medium text-gray-700 mb-1">
-                STD (Must be unique)
+                STD
               </label>
               <input
                 type="text"
