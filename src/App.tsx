3b112a892e077447ec3cb39b7fa480e3556ecbdc
Jessy Randriamizara <henintsoarandriam@gmail.com>
2025-06-04 10:55:08 +0300

    chore: route handler (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import AuthForm from './components/AuthForm';
+import { Route, Routes } from 'react-router';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <AuthForm />
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
   );
 }
