7d6413a20ee5032986c6dbd3c82610dcc19c13da
Fiantso Harena <fiantsorav@gmail.com>
2025-06-04 12:01:50 +0300

    chore: create a simple article to present the platform

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router';
 import Login from './pages/login';
 import Home from './pages/home';
+import Auth from './pages/auth';
 
 export default function App() {
   return (
     <Routes>
+      <Route path="/auth" element={<Auth />} />
       <Route path="/login" element={<Login />} />
       <Route path="/home" element={<Home />} />
     </Routes>
